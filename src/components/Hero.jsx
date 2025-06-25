58e5b8ba807983385c9dd863d461646d251c503f
Cootzy <umastryxsubterror@gmail.com>
2025-06-25 19:32:19 +0700

    Implement Hero Scroll Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,31 @@
 import { useGSAP } from "@gsap/react";
-import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import React from "react";
+import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  useGSAP(() => {
-    const heroSplit = new SplitText(".title", { type: "chars, words" });
-    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+  const videoRef = useRef();
 
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
+
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    //* Apply text-gradient class once before animating
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
       ease: "expo.out",
-      stagger: 0.1,
+      stagger: 0.06,
     });
 
     gsap.from(paragraphSplit.lines, {
@@ -22,7 +33,7 @@ const Hero = () => {
       yPercent: 100,
       duration: 1.8,
       ease: "expo.out",
-      stagger: 0.1,
+      stagger: 0.06,
       delay: 1,
     });
 
@@ -36,7 +47,27 @@ const Hero = () => {
         },
       })
       .to(".right-leaf", { y: 200 }, 0)
-      .to(".left-leaf", { y: -200 }, 0);
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -49,7 +80,6 @@ const Hero = () => {
           alt="left-leaf"
           className="left-leaf"
         />
-
         <img
           src="/images/hero-right-leaf.png"
           alt="right-leaf"
@@ -57,9 +87,11 @@ const Hero = () => {
         />
 
         <div className="body">
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+
           <div className="content">
             <div className="space-y-5 hidden md:block">
-              <p>Cool. Crisp. Classic</p>
+              <p>Cool. Crisp. Classic.</p>
               <p className="subtitle">
                 Sip the Spirit <br /> of Summer
               </p>
@@ -68,14 +100,24 @@ const Hero = () => {
             <div className="view-cocktails">
               <p className="subtitle">
                 Every cocktail on our menu is a blend of premium ingredients,
-                creative flair, and timeless recipes - designed to delight your
+                creative flair, and timeless recipes — designed to delight your
                 senses.
               </p>
-              <a href="cocktails">View Cocktails</a>
+              <a href="#cocktails">View cocktails</a>
             </div>
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/output.mp4"
+        />
+      </div>
     </>
   );
 };
